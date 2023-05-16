b77abd5bc3aed987f5cebc5743fba1d920475e3a
Sunray8055 <biradarravikiran379@gmail.com>
2023-05-16 15:33:45 +0530

    Commiting Test 2 branch Ravi

---
+++ src/Test2.java
@@ -0,0 +1,24 @@
+
+public class Test2 {
+
+	public static void main(String[] args)
+	{
+		int temp,i,j;
+		int arr[]= {50,30,10,20,60,40};
+		System.out.print("Ascending order: ");
+		for(i=0;i<arr.length;i++)
+		{
+			for(j=i+1;j<arr.length;j++) 
+			{
+				if(arr[i]>arr[j]) 
+				{
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+			System.out.print(arr[i]+",");
+		}	
+	} 
+
+}
