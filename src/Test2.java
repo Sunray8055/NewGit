82a29f49c6e4950ed2a6e5912975172c80330391
Sunray8055 <biradarravikiran379@gmail.com>
2023-05-16 16:07:28 +0530

    Added checks

--- src/Test2.java
+++ src/Test2.java
@@ -4,8 +4,11 @@ public class Test2 {
 	public static void main(String[] args)
 	{
 		int temp,i,j;
+		
 		int arr[]= {50,30,10,20,60,40};
+		
 		System.out.print("Ascending order: ");
+		
 		for(i=0;i<arr.length;i++)
 		{
 			for(j=i+1;j<arr.length;j++) 
